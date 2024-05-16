bead312042ac209babf8375aecef0bf4e6387daf
Diving Island <39563325+divingisland@users.noreply.github.com>
2024-05-16 00:14:42 +0000

    Update test.js

--- js/test.js
+++ js/test.js
@@ -14,6 +14,6 @@ window.onclick = function(e) {
 	if(parent.className && parent.parentNode(".menu-list")) {
     var elText = el.textContent;
     	/*return elText;*/
-		console.Log(elText);
+		console.log(elText);
     }
 }
